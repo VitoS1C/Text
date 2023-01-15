f8400d15dd795b59b210a964bdc5a467d6a95258
vitos <vitos@SHPET>
2023-01-15 12:40:38 +0500

    Добавьте файлы проекта.

---
+++ Text/Program.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Text
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			ChooseOperation(out char operationType);
+
+			switch (operationType)
+			{
+				case '1':
+					CreateFile();
+					break;
+
+				case '2':
+					ChooseFile();
+					break;
+
+				default:
+					break;
+			}
+
+		}
+		static void ChooseOperation(out char operationType)
+		{
+			while (true)
+			{
+				Console.WriteLine("Что хотите сделать?");
+				Console.Write("1 - Создать файл, 2 - Получить список файлов: ");
+				string userInput = Console.ReadLine() ?? "";
+
+				if (userInput == "1" || userInput == "2")
+				{
+					operationType = userInput[0];
+					break;
+				}
+				else
+				{
+					WrongInput("Недопустимая команда!");
+				}
+
+			}
+		}
+		
+		static void CreateFile()
+		{
+			Console.Write("Введите имя файла: ");
+			string path = $"{Console.ReadLine()}.txt";
+			Console.WriteLine("\n");
+
+			TextEditor textEditor = new(path);
+			textEditor.WriteText();
+		}
+
+		static void ChooseFile()
+		{
+
+			DirectoryInfo directory = new(Environment.CurrentDirectory);
+			FileInfo[] files = directory.GetFiles("*.txt");
+
+			Console.WriteLine("\nВыбырите какой файл открыть\n");
+
+			for (int i = 0; i < files.Length; i++)
+			{
+				if (i == files.Length - 1)
+				{
+					Console.Write($"{i + 1} - {files[i].Name} --> ");
+				}
+				else
+					Console.WriteLine($"{i + 1} - {files[i].Name}");
+			}
+
+			int userInput;
+
+			while (true)
+			{
+				int.TryParse(Console.ReadLine(), out userInput);
+
+				if (userInput > files.Length - 1 || userInput == 0)
+				{
+					WrongInput("Такого файла нет!");
+				}
+				else
+				{
+					Console.WriteLine('\n');
+
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine($"{Path.GetFileNameWithoutExtension(files[userInput - 1].FullName)}\n");
+					Console.ForegroundColor = ConsoleColor.Gray;
+
+					TextEditor textEditor = new($"{files[userInput - 1].Name}");
+					textEditor.ReadText();
+				}
+			}
+
+
+		}
+
+		static void WrongInput(string text)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"\n{text}\n");
+			Console.ForegroundColor = ConsoleColor.Gray;
+		}
+	}
+
+}
---
+++ Text/TextEditor.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Text
+{
+	internal class TextEditor
+	{
+		public string Path { get; }
+
+		public TextEditor(string path)
+		{
+			Path = path;
+		}
+
+		public async void WriteText()
+		{
+			Console.WriteLine("Вводите текст. Чтобы закончить печать, наберите \"Quit\"");
+
+			FileStream fs = new(Path, FileMode.OpenOrCreate);
+			fs.Close();
+
+			while (true)
+			{
+				using StreamWriter streamWriter = new(Path, true);
+				string userInput = Console.ReadLine() ?? "";
+
+				if (userInput.ToLower() == "quit")
+				{
+					break;
+				}
+				else
+				{
+					await streamWriter.WriteLineAsync(userInput);
+
+				}
+
+			}
+			
+		}
+
+		public void ReadText()
+		{
+			string[] text = File.ReadAllLines(Path);
+
+			for (int i = 0; i < text.Length; i++)
+			{
+				Console.WriteLine(text[i]);
+			}
+
+		}
+
+	}
+}
